94e91786757008be6a81b4bf3c0dce28202a1b0c
muhammadnaufalopet <141712648+muhammadnaufalopet@users.noreply.github.com>
2025-01-06 10:39:22 +0700

    Add files via upload

---
+++ pages/pages/biodata.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container-fluid text-center">
+    <div class="row">
+      <div class="col-3">
+        <h4>Biodata</h4>
+        <div class="shadow-sm p-3 mb-5 bg-body-tertiary rounded">
+          <img src="/assets/img/15.jpg" style="width: 15rem;" alt="">
+
+          <p>Nama: Muhammad Naufal</p>
+          <p>Alamat: Kp.Tanjung Wangi</p>
+          <p>Jenis Kelamin: Laki laki</p>
+          <p>Gmail: 222310267@smkn4-tsm.sch.id</p>
+          <p>No. Hp: 083152331501</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
